bb59c30e049cdd5fd4fdca2ab4ecfd17b525a9c8
BonkersDenis <118104588+BonkersDenis@users.noreply.github.com>
2023-10-20 08:36:52 +0300

    Добавьте файлы проекта.

---
+++ myEsin/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace myEsin
+{
+    public class Graph
+    {
+        private bool _directed;
+        private Dictionary<string, List<(string, int)>> _adjacencyList;
+        
+        // Конструкторы
+        public Graph(bool directed = false)
+        {
+            _directed = directed;
+            _adjacencyList = new Dictionary<string, List<(string, int)>>();
+        }
+
+        public Graph(string fileName)
+        {
+            //LoadFromFile(fileName);
+        }
+
+        public Graph(Graph other)
+        {
+            _directed = other._directed;
+            _adjacencyList = new Dictionary<string, List<(string, int)>>(other._adjacencyList);
+        }
+
+        // Методы
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
